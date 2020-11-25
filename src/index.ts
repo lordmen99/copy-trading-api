527623fdfb6e393d6ef6cf61ef7a96326fcaf449
manhninh91 <manhninh91@gmail.com>
2020-11-26 02:17:56 +0700

    Update code socket connect trading server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import http from 'http';
 import 'module-alias/register';
 import mongoose from 'mongoose';
 import {Server} from 'socket.io';
+import {Manager} from 'socket.io-client';
 import app from './App';
 import config from './config';
 import socketIoHandlers from './socketHandlers/EventHandlers';
@@ -51,20 +52,20 @@ server.on('error', (error: NodeJS.ErrnoException): void => {
   }
 });
 
-// const manager = new Manager("ws://localhost:5000/", { path: "/io-copy-trading" });
-// const socket = manager.socket("/");
+const manager = new Manager('ws://localhost:6789/', {path: '/kline'});
+const socket = manager.socket('/');
 
-// socket.on("connect", () => {
-//   console.log(`connect ${socket.id}`);
-// });
+socket.on('connect', () => {
+  console.log(`connect ${socket.id}`);
+});
 
-// socket.on("disconnect", () => {
-//   console.log(`disconnect`);
-// });
+socket.on('disconnect', () => {
+  console.log(`disconnect`);
+});
 
-// setInterval(() => {
-//   const start = Date.now();
-//   socket.emit("ping", () => {
-//     console.log(`pong (latency: ${Date.now() - start} ms)`);
-//   });
-// }, 1000);
+setInterval(() => {
+  const start = Date.now();
+  socket.emit('ping', () => {
+    console.log(`pong (latency: ${Date.now() - start} ms)`);
+  });
+}, 1000);
--- src/socketHandlers/EventHandlers.ts
+++ src/socketHandlers/EventHandlers.ts
@@ -1,3 +1,4 @@
+import logger from '@src/middleware/Logger';
 import {Server} from 'socket.io';
 import TradingSystem from './events/TradingSystem';
 import {AppData, Socket} from './EventTypes';
@@ -7,23 +8,36 @@ const app: AppData = {
 };
 
 export default (io: Server) => {
-  io.on('connection', (socket: Socket<any, any>) => {
-    console.log('Connection Successfull');
-    const eventHandlers = [TradingSystem(app, socket)];
-    eventHandlers.forEach((handler) => {
-      for (const eventName in handler) {
-        socket.on(eventName, handler[eventName]);
-      }
+  try {
+    io.on('connect', (socket: Socket<any, any>) => {
+      console.log('Connection Successfull 2222');
     });
-    app.allSockets.push(socket);
-  });
-  io.on('connect_failed', () => {
-    console.log('Connection Failed');
-  });
-  io.on('disconnect', () => {
-    console.log('Disconnected');
-  });
-  io.on('Error ', () => {
-    console.log('Disconnected');
-  });
+
+    io.on('connection', (socket: Socket<any, any>) => {
+      console.log('Connection Successfull');
+      const eventHandlers = [TradingSystem(app, socket)];
+      eventHandlers.forEach((handler) => {
+        for (const eventName in handler) {
+          socket.on(eventName, handler[eventName]);
+        }
+      });
+      app.allSockets.push(socket);
+    });
+
+    io.on('connect_failed', () => {
+      console.log('Connection Failed');
+    });
+
+    io.on('disconnect', () => {
+      console.log('Disconnected');
+    });
+
+    io.on('Error ', () => {
+      console.log('Disconnected');
+    });
+  } catch (error) {
+    logger.error(`-----------------------BEGIN ERROR-------------------------------`);
+    logger.error(`SOCKET ERROR: ${error.message}`);
+    logger.error(`-----------------------END ERROR---------------------------------`);
+  }
 };
