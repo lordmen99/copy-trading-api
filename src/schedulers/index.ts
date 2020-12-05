cf1f4db767a248f3241d503731bb3986e445045f
danh.nguyenminh <danhnm@chipo.vn>
2020-12-05 10:40:59 +0000

    config env for schedule

--- src/schedulers/index.ts
+++ src/schedulers/index.ts
@@ -1,12 +1,12 @@
+import config from '@src/config';
 import scheduler from 'node-schedule';
 import executeTransferFeeToExpert from './executeTransferFeeToExpert';
 import executeUnblockUser from './executeUnblockUser';
 import executeWithdrawAfterStopCopy from './executeWithdrawAfterStopCopy';
 export default class Scheduler {
   public config() {
-    scheduler.scheduleJob('*/30 * * * * *', executeUnblockUser);
-    // scheduler.scheduleJob('0 59 23 * * *', executeTransferFeeToExpert);
-    scheduler.scheduleJob('0 */30 * * * *', executeTransferFeeToExpert); // setup 30 minutes to test
-    scheduler.scheduleJob('*/30 * * * * *', executeWithdrawAfterStopCopy); // setup 30 minutes to test
+    scheduler.scheduleJob(config.SCHEDULE_EXECUTE_UNBLOCK_USER, executeUnblockUser);
+    scheduler.scheduleJob(config.SCHEDULE_EXECUTE_TRANSFER_FEE_TO_EXPERT, executeTransferFeeToExpert);
+    scheduler.scheduleJob(config.SCHEDULE_EXECUTE_WITHDRAW_AFTER_STOP_COPY, executeWithdrawAfterStopCopy);
   }
 }
