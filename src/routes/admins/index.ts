2f22fc24a3d7decb8d6f0229179a3b8184a0f575
ninh.pham <manhninh91@gmail.com>
2020-12-14 18:47:37 +0700

    udpate code

--- src/routes/admins/index.ts
+++ src/routes/admins/index.ts
@@ -1,11 +1,10 @@
 import {Router} from 'express';
 import changePasswordAdmin from './ChangePasswordAdmin';
-import createAdmin from './CreateAdmin';
 export default class UserRouter {
   public router: Router = Router();
 
   constructor() {
-    createAdmin(this.router);
+    // createAdmin(this.router);
     changePasswordAdmin(this.router);
   }
 }
